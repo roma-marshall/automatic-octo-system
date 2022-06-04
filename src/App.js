eaa4d7327f10b4536cffb95e8505e9be2bc24edf
roma_marshall <roma_marshall@outlook.com>
2022-06-04 20:56:29 +0200

    set default value

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import store from "./store";
 class App extends React.Component {
     constructor() {
         super();
+        this.state = {
+            out: '0'
+        }
     }
 
     render() {
@@ -12,12 +15,13 @@ class App extends React.Component {
                 <div className="output">
                     <input
                         type="text"
-                        defaultValue="0"
+                        defaultValue={this.state.out}
                     />
                     <div className="buttons">
-                        {store.buttons.map((item) => {
-                            return <button className="font-bold ml-3 py-2 px-4 rounded text-white bg-amber-500 hover:bg-amber-600/90">{item.val}</button>
-                        })}
+                        {store.buttons.map((item) =>
+                            <button
+                                className="font-bold ml-3 py-2 px-4 rounded text-white bg-amber-500 hover:bg-amber-600/90">{item.val}</button>
+                        )}
                     </div>
                 </div>
             </div>
